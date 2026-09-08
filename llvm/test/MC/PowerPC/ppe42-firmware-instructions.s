c4a45e591b543889afa50b6f11080cbd5c8a1b4b
Manojkiran Eda <manojkiran.eda@gmail.com>
2026-09-08 23:02:44 +0530

    PowerPC: accept PPE42 mtcr0 mnemonic

--- llvm/test/MC/PowerPC/ppe42-firmware-instructions.s
+++ llvm/test/MC/PowerPC/ppe42-firmware-instructions.s
@@ -8,6 +8,7 @@
         subwibnz 4, 1, -4
         mtedr 2
         mtdbcr 5
+        mtcr0 3
         lvd 1, 0x2040(4)
         lvdu 5, 8(10)
         stvdu 6, 8(9)
@@ -20,6 +21,7 @@
 # CHECK: subwibnz 4, 1, {{.*}}encoding: [0x06,0x64,0x0f,0xfe]
 # CHECK: mtedr 2{{.*}}encoding: [0x7c,0x5d,0x0b,0xa6]
 # CHECK: mtdbcr 5{{.*}}encoding: [0x7c,0xb4,0x4b,0xa6]
+# CHECK: mtcr0 3{{.*}}encoding: [0x7c,0x68,0x01,0x20]
 # CHECK: lvd 1, 8256(4){{.*}}encoding: [0x14,0x24,0x20,0x40]
 # CHECK: lvdu 5, 8(10){{.*}}encoding: [0x24,0xaa,0x00,0x08]
 # CHECK: stvdu 6, 8(9){{.*}}encoding: [0x58,0xc9,0x00,0x08]
