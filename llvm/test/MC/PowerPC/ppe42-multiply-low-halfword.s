5cdbec3950c988f4e4af58cf57d6374fbf9d7f7f
Manojkiran Eda <manojkiran.eda@gmail.com>
2026-09-22 16:21:43 +0530

    [PowerPC] Fix PPE42 mullhw encoding

--- llvm/test/MC/PowerPC/ppe42-multiply-low-halfword.s
+++ llvm/test/MC/PowerPC/ppe42-multiply-low-halfword.s
@@ -1,8 +1,9 @@
 # RUN: llvm-mc -triple=powerpc-unknown-elf -mcpu=ppe42 -show-encoding %s | FileCheck %s
 
-# PPE42 User's Manual, section 9.4.59: XO-form opcode 4, XO=392.
-# CHECK: mullhw r3, r4, r5                  # encoding: [0x10,0x64,0x28,0x10]
-# CHECK: mullhw. r3, r4, r5                # encoding: [0x10,0x64,0x28,0x11]
+# PPE42 User's Manual, sections 9.4.58-59: XO-form opcode 4,
+# XO=424/392 for signed/unsigned forms.
+# CHECK: mullhw r3, r4, r5                  # encoding: [0x10,0x64,0x29,0x10]
+# CHECK: mullhw. r3, r4, r5                # encoding: [0x10,0x64,0x29,0x11]
 # CHECK: mullhwu r6, r7, r8                # encoding: [0x10,0xc7,0x40,0x10]
 # CHECK: mullhwu. r6, r7, r8              # encoding: [0x10,0xc7,0x40,0x11]
 
